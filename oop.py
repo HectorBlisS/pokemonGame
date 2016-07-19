98baa02cacca9f57b84b08396965ff68a8d40d8b
Topi <nokoayzack@gmail.com>
2016-07-19 12:51:20 -0500

    Agregamos nuevos ataques para todos los tipos y la propiead de furia que permite hacer ataques mas fuertes

--- oop.py
+++ oop.py
@@ -6,37 +6,68 @@ class Pokemon(object):
 		self.weight = 20
 		self.nombre = nombre
 		self.vida = 100
-
+		self.furia = 0
 
 	def hablar(self):
 		""" Este metodo imprime el nombre en pantalla """
 		print(self.nombre[:4],self.nombre)
 
+	def golpear(self,atacado):
+		""" Metodo del golpe mas simple de todos """ 
+		self.checar_vida()
+		print("Golpeee!!!")
+		atacado.vida -= 10
+		atacado.furia += 10
+		atacado.checar_vida()
+
+	def apachurrar(self,atacado):
+		""" """
+		if self.vida <50 and self.furia>50:
+			self.checar_vida()
+			print('como se mata el gusano?')
+			atacado.vida -= 50
+			self.furia -= 50
+			atacado.furia += 50
+		else:
+			print('no puedo')
+		atacado.checar_vida()
+
+	def fatality(self,atacado):
+		if self.vida <= 5 and self.furia>=100:
+			atacado.vida -= 80
+			self.furia -= 80
+			print("Fataliiiiityyyyy")
+			atacado.checar_vida()
+		else:
+			print("No puedo")
+
+	def burlarse(self, atacado):
+		print("Ni cosquillas me haces :P")
+		atacado.furia += 15
+		self.vida += 10
+
 	def checar_vida(self):
 		if self.vida <=0:
 			print('Me petatie')
+		else:
+			print(self.nombre.capitalize(),"\n    Vida:",self.vida,"\n    Furia:",self.furia)
 
 class TipoFuego(Pokemon):
 	""" Esta clase hereda de Pokemon y agrega escupir """
 	def escupir(self,atacado):
 		""" escupe lumbre """
+		self.checar_vida()
 		print('Toma tu lumbre!')
 		atacado.vida -=30
+		atacado.furia += 30
 		atacado.checar_vida()
-
-	def apachurrar(self,atacado):
-		if self.vida <50:
-			print('como se mata el gusano?')
-			atacado.vida -=50
-			atacado.checar_vida()
-		else:
-			print('no puedo')
-
 
 class TipoAgua(Pokemon):
 	def chorriar(self,atacado):
+		self.checar_vida()
 		print('mojate!')
 		atacado.vida -=30
+		atacado.furia +=30
 		atacado.checar_vida()
 
 
